cca037e1cf5c3ddf35589734b78178352b9edb13
Pavel Puchkov <0x6368656174@gmail.com>
2015-07-04 10:17:12 +0900

    Update fontawesomechars.js

--- fontawesomechars.js
+++ fontawesomechars.js
@@ -1,20 +1,20 @@
 /**********************************************************************************
  * Copyright © 2015 Pavel A. Puchkov                                              *
  *                                                                                *
- * This file is part of FontAwesomeGetChar.                                       *
+ * This file is part of FontAwesomeChars.                                         *
  *                                                                                *
- * FontAwesomeGetChar is free software: you can redistribute it and/or modify     *
+ * FontAwesomeChars is free software: you can redistribute it and/or modify       *
  * it under the terms of the GNU Lesser General Public License as published by    *
  * the Free Software Foundation, either version 3 of the License, or              *
  * (at your option) any later version.                                            *
  *                                                                                *
- * FontAwesomeGetChar is distributed in the hope that it will be useful,          *
+ * FontAwesomeChars is distributed in the hope that it will be useful,            *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  *
  * GNU Lesser General Public License for more details.                            *
  *                                                                                *
  * You should have received a copy of the GNU Lesser General Public License       *
- * along with FontAwesomeGetChar.  If not, see <http://www.gnu.org/licenses/>.    *
+ * along with FontAwesomeChars.  If not, see <http://www.gnu.org/licenses/>.      *
  **********************************************************************************/
 
 function getChar(charName) {
